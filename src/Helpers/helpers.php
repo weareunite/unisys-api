f53cab2560ff4b24ead4dff32fe8e83e0b066668
Vlado Zilka <vlado.zilka@gmail.com>
2018-07-23 13:30:53 +0200

    add helpers

---
+++ src/Helpers/helpers.php
@@ -0,0 +1,57 @@
+<?php
+
+if(! function_exists('remove_trailing_zero'))
+{
+    function remove_trailing_zero(float $value, $decimal_point = '.')
+    {
+        return str_replace($decimal_point."00", "", (string)number_format ($value, 2, $decimal_point, ""));
+    }
+}
+
+if(! function_exists('price_without_tax'))
+{
+    function price_without_tax($price_with_tax, $tax, bool $round = true)
+    {
+        $price_without_tax = $price_with_tax / (1 + ( $tax / 100 ));
+
+        if($round === true) {
+            $price_without_tax = round($price_without_tax, 2);
+        }
+
+        return $price_without_tax;
+    }
+}
+
+if(! function_exists('locale_file_path'))
+{
+    function locale_file_path(string $path, string $locale, string $callback_locale = null)
+    {
+        $localeFilePath = storage_path('app/' . sprintf($path, $locale));
+
+        if(!file_exists($localeFilePath) && !$callback_locale) {
+            return null;
+        }
+
+        if(!file_exists($localeFilePath) && $callback_locale) {
+            $localeFilePath = storage_path('app/' . sprintf($path, $callback_locale));
+        }
+
+        if(!file_exists($localeFilePath)) {
+            return null;
+        }
+
+        return $localeFilePath;
+    }
+}
+
+if(! function_exists('short_locale_file_path'))
+{
+    function get_short_locale_file_path(string $path, string $locale, string $callback_locale = null)
+    {
+        $full_path = locale_file_path($path, $locale, $callback_locale);
+
+        $app_path = storage_path('app/');
+
+        return str_replace($app_path, '', $full_path);
+    }
+}
