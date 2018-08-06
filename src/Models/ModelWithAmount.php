50cfd3cc4a8fdf75608116af87d636131adba6e8
Vlado Zilka <vlado.zilka@gmail.com>
2018-08-06 16:11:56 +0200

    c

---
+++ src/Models/ModelWithAmount.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Unite\UnisysApi\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property float|null $amount
+ */
+abstract class ModelWithAmount extends Model
+{
+
+}
---
+++ src/Services/AmountService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Unite\UnisysApi\Services;
+
+use Illuminate\Database\Eloquent\Model;
+use Unite\UnisysApi\Models\ModelWithAmount;
+
+class AmountService extends AbstractService
+{
+    const DIRECTION_INCREASE = 'increase';
+    const DIRECTION_DECREASE = 'decrease';
+    const DIRECTION_DECIDE = 'decide';
+
+    public static function getTypes(): array
+    {
+        return [
+            self::DIRECTION_INCREASE,
+            self::DIRECTION_DECREASE,
+            self::DIRECTION_DECIDE,
+        ];
+    }
+
+    public function updateAttributeByAmount(Model $model, string $attribute, ModelWithAmount $source, string $type)
+    {
+        if ($type === self::DIRECTION_INCREASE) {
+            $model->{$attribute} += $source->amount;
+        } elseif ($type === self::DIRECTION_DECREASE) {
+            $model->{$attribute} -= $source->amount;
+        } elseif ($type === self::DIRECTION_DECIDE) {
+            $original = $source->getOriginal();
+
+            if (isset($original['amount']) && $source->amount !== $original['amount']) {
+                $model->{$attribute} -= $original['amount'];
+                $model->{$attribute} += $source->amount;
+            }
+        }
+
+        $model->save();
+    }
+}
