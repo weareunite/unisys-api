ef9905f6aaf1cef35fc742f9a83179ee70a6ce91
Vlado Zilka <vlado.zilka@gmail.com>
2019-07-29 15:47:06 +0200

    fix

--- src/Modules/Users/src/Console/Commands/SetFirstUser.php
+++ src/Modules/Users/src/Console/Commands/SetFirstUser.php
@@ -48,22 +48,16 @@ class SetFirstUser extends Command
     /** @var  string */
     protected $plainPassword;
 
-    /*
-     * Execute the console command.
-     */
-    public function __construct(CountryRepository $countryRepository, SettingService $settingService, InstanceService $instanceService)
-    {
-        parent::__construct();
 
+    public function handle(CountryRepository $countryRepository, SettingService $settingService, InstanceService $instanceService)
+    {
         $this->countryRepository = $countryRepository;
         $this->settingService = $settingService;
         $this->instanceService = $instanceService;
-    }
 
-    public function handle()
-    {
-        if(User::count() > 0) {
+        if (User::count() > 0) {
             $this->info('Table of users is not empty ...');
+
             return;
         }
 
@@ -82,43 +76,43 @@ class SetFirstUser extends Command
         $this->getOutput()->success('DONE! You can now log in to your API.');
 
         $this->getOutput()->note('REMEMBER Login credentials');
-        $this->table(['Username', 'Password'], [[$this->user->username, $this->plainPassword]]);
+        $this->table([ 'Username', 'Password' ], [ [ $this->user->username, $this->plainPassword ] ]);
     }
 
     private function createInstance()
     {
         $this->getOutput()->title('Create Instance');
 
-        $data['name'] = $this->validate(function() {
+        $data['name'] = $this->validate(function () {
             return $this->ask('Instance name *');
         }, 'required|string|max:150');
 
         $this->instance = Instance::create($data);
 
-        $this->getOutput()->success('Instance '. $this->instance->name .' was created');
+        $this->getOutput()->success('Instance ' . $this->instance->name . ' was created');
     }
 
     private function createUser()
     {
         $this->getOutput()->title('Create User');
 
-        $data['name'] = $this->validate(function() {
+        $data['name'] = $this->validate(function () {
             return $this->ask('Name *');
         }, 'required|string|max:100');
 
-        $data['surname'] = $this->validate(function() {
+        $data['surname'] = $this->validate(function () {
             return $this->ask('Surname *');
         }, 'required|string|max:100');
 
-        $data['email'] = $this->validate(function() {
+        $data['email'] = $this->validate(function () {
             return $this->ask('Email *');
         }, 'required|email');
 
-        $data['username'] = $this->validate(function() {
+        $data['username'] = $this->validate(function () {
             return $this->ask('Username for login *');
         }, 'required|string|max:100');
 
-        $this->plainPassword = $data['password'] = $this->validate(function() {
+        $this->plainPassword = $data['password'] = $this->validate(function () {
             return $this->ask('Password for login *');
         }, 'required|string|max:100');
 
@@ -128,18 +122,18 @@ class SetFirstUser extends Command
 
         $this->instanceService->setUser($this->user);
 
-        $this->getOutput()->success('User '. $this->user->getFullName() .' was created');
+        $this->getOutput()->success('User ' . $this->user->getFullName() . ' was created');
     }
 
     private function createDefaultRoles()
     {
         $this->adminRole = Role::create([
-            'name' => 'admin',
+            'name'       => 'admin',
             'guard_name' => 'api',
         ]);
 
         Role::create([
-            'name' => 'user',
+            'name'       => 'user',
             'guard_name' => 'api',
         ]);
 
@@ -150,65 +144,65 @@ class SetFirstUser extends Command
     {
         $this->user->assignRole($this->adminRole);
 
-        $this->info('User '. $this->user->getFullName() .' was attached to role Admin');
+        $this->info('User ' . $this->user->getFullName() . ' was attached to role Admin');
     }
 
     private function attachUserToInstance()
     {
         $this->user->instances()->attach($this->instance->id);
 
-        $this->info('User '. $this->user->getFullName() .' was attached to instance '. $this->instance->name);
+        $this->info('User ' . $this->user->getFullName() . ' was attached to instance ' . $this->instance->name);
     }
 
     private function setCompanyProfile()
     {
         $this->getOutput()->title('Set Company information');
 
-        $contactData['name'] = $this->validate(function() {
+        $contactData['name'] = $this->validate(function () {
             return $this->ask('Contact name *');
         }, 'required|string|max:40');
 
-        $contactData['company'] = $this->validate(function() {
+        $contactData['company'] = $this->validate(function () {
             return $this->ask('Company name *');
         }, 'required|string|max:40');
 
-        $contactData['street'] = $this->validate(function() {
+        $contactData['street'] = $this->validate(function () {
             return $this->ask('Street');
         }, 'nullable|string|max:40');
 
-        $contactData['zip'] = $this->validate(function() {
+        $contactData['zip'] = $this->validate(function () {
             return $this->ask('Zip');
         }, 'nullable|string|max:10');
 
-        $contactData['city'] = $this->validate(function() {
+        $contactData['city'] = $this->validate(function () {
             return $this->ask('City');
         }, 'nullable|string|max:20');
 
-        $countryName = $this->validate(function() {
+        $countryName = $this->validate(function () {
             return $this->choice('Country', $this->countryRepository->getListForSelect(), Country::DEFAULT_COUNTRY_ID);
         }, 'string');
 
-        if($countryName && $country = $this->countryRepository->getByName($countryName)) {
+        if ($countryName && $country = $this->countryRepository->getByName($countryName)) {
             $contactData['country_id'] = $country->id;
         }
 
-        $contactData['reg_no'] = $this->validate(function() {
+        $contactData['reg_no'] = $this->validate(function () {
             return $this->ask('Registration number');
         }, 'nullable|string|max:20');
 
-        $contactData['tax_no'] = $this->validate(function() {
+        $contactData['tax_no'] = $this->validate(function () {
             return $this->ask('Tax number');
         }, 'nullable|string|max:20');
 
-        $contactData['vat_no'] = $this->validate(function() {
+        $contactData['vat_no'] = $this->validate(function () {
             return $this->ask('Vat number');
         }, 'nullable|string|max:20');
 
-        $contactData['email'] = $this->validate(function() {
+        $contactData['email'] = $this->validate(function () {
             return $this->ask('Email *');
         }, 'required|email');
 
-        $contactData['telephone'] = $this->validate(function() {
+        $contactData['telephone'] = $this->validate(function () {
             return $this->ask('Phone number');
         }, 'nullable|string|max:20');
 
@@ -222,10 +216,10 @@ class SetFirstUser extends Command
         do {
             $value = $closure();
 
-            $validator = \Validator::make(['input' => $value], ['input' => $validates]);
+            $validator = \Validator::make([ 'input' => $value ], [ 'input' => $validates ]);
 
             if ($validator->fails()) {
-                $this->error( $validator->errors()->first() );
+                $this->error($validator->errors()->first());
             }
         } while ($validator->fails());
 
