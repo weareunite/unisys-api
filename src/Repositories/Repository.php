b3b865dadb8ee2a689808811c14dae0a3687f79d
Vlado Zilka <vlado.zilka@gmail.com>
2020-03-10 20:13:24 +0100

    improve repository

--- src/Repositories/Repository.php
+++ src/Repositories/Repository.php
@@ -3,147 +3,47 @@
 namespace Unite\UnisysApi\Repositories;
 
 use Illuminate\Container\Container;
-use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Query\Builder as QueryBuilder;
 use Unite\UnisysApi\Contracts\Repository as RepositoryContract;
 
 abstract class Repository implements RepositoryContract
 {
-    /**
-     * @var Model;
-     */
-    protected $model;
+    /** @var Builder */
+    protected $query;
 
+    /** @var string */
     protected $modelClass;
 
+    /** @var Model */
+    protected $model;
+
+    /**
+     * Repository constructor.
+     * @param Container $app
+     * @throws BindingResolutionException
+     */
     public function __construct(Container $app)
     {
-        if (!class_exists($this->modelClass)) {
-            $this->loadModelClass();
-        }
-
         $this->model = $app->make($this->modelClass);
     }
 
-    protected function loadModelClass()
-    {
-        [$class] = array_slice(array_reverse(explode('\\', get_called_class())), 0, 2);
-
-        $class = str_replace('Repository', '', $class);
-
-        $this->setModelClass('App\\Models\\' . $class);
-
-        return $this;
-    }
-
-    protected function setModelClass($class)
-    {
-        $this->modelClass = $class;
-
-        return $this;
-    }
-
-    public function getModelClass(): string
+    public function getModelClass()
+    : string
     {
         return $this->modelClass;
     }
 
-    /**
-     * Provides QueryBuilder
-     *
-     * @return EloquentBuilder|QueryBuilder
-     */
-    public function getQueryBuilder()
+    public function newQuery()
+    : Builder
     {
-        return $this->model;
+        return $this->query = $this->model->newQuery();
     }
 
     public function getTable()
+    : string
     {
-        return $this->model->getModel()->getTable();
-    }
-
-    public function find($id, $columns = ['*'])
-    {
-        return $this->model->find($id, $columns);
-    }
-
-    public function create(array $attributes = [])
-    {
-        $result = $this->model->create($attributes);
-
-        return $result;
-    }
-
-    public function forceCreate(array $attributes = [])
-    {
-        $result = $this->model->forceCreate($attributes);
-
-        return $result;
-    }
-
-    public function update(array $values)
-    {
-        $result = $this->model->update($values);
-
-        return $result;
-    }
-
-    public function updateForId($id, array $values)
-    {
-        $result = $this->model->where('id', '=', $id)->update($values);
-
-        return $result;
-    }
-
-    public function delete($id)
-    {
-        $result = $this->model->destroy($id);
-
-        return $result;
-    }
-
-    public function massDelete(array $ids)
-    {
-        $result = null;
-
-        foreach ($ids as $id) {
-            $result = $this->model->destroy($id);
-        }
-
-        return $result;
-    }
-
-    public function join($table, $first, $operator = null, $second = null, $type = 'inner', $where = false)
-    {
-        return $this->model->join($table, $first, $operator, $second, $type, $where);
-    }
-
-    public function where($column, $operator = null, $value = null, $boolean = 'and')
-    {
-        return $this->model->where($column, $operator, $value, $boolean);
-    }
-
-    public function orWhere($column, $operator = null, $value = null)
-    {
-        return $this->model->orWhere($column, $operator, $value);
-    }
-
-    public function get($columns = ['*'])
-    {
-        return $this->model->get($columns);
-    }
-
-    public function paginate($perPage = null, $columns = ['*'], $pageName = 'page', $page = null)
-    {
-        return $this->model->paginate($perPage, $columns, $pageName, $page);
-    }
-
-    public function with($relations)
-    {
-        $this->model = $this->model->with($relations);
-
-        return $this;
+        return $this->model->getTable();
     }
 }
