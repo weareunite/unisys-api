58e796b0a6a4a426f3e12448809dc2f2fbd73e85
Vlado Zilka <vlado.zilka@gmail.com>
2020-03-12 08:37:58 +0100

    update install module command

--- src/Console/InstallModuleCommand.php
+++ src/Console/InstallModuleCommand.php
@@ -3,22 +3,17 @@
 namespace Unite\UnisysApi\Console;
 
 use Illuminate\Console\Command;
-use Unite\UnisysApi\Repositories\InstalledModuleRepository;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Facades\File;
 
-abstract class InstallModuleCommand extends Command implements InstallModuleCommandInterface
+abstract class InstallModuleCommand extends Command
 {
     protected $moduleName;
 
-    protected $installedModuleRepository;
-
     protected $fileSystem;
 
-    public function __construct(InstalledModuleRepository $installedModuleRepository, Filesystem $files)
+    public function __construct(Filesystem $files)
     {
-        $this->installedModuleRepository = $installedModuleRepository;
-
         $this->signature = 'unisys-api:install:' . $this->moduleName;
 
         $this->description = 'Install [' . $this->moduleName . '] module to Unisys API';
@@ -27,37 +22,25 @@ abstract class InstallModuleCommand extends Command implements InstallModuleComm
 
         parent::__construct();
     }
+
     /*
      * Execute the console command.
      */
     public function handle()
     {
-        $this->info('Installing module '. $this->moduleName .' ...');
+        $this->info('Installing module ' . $this->moduleName . ' ...');
 
-        if($this->installedModuleRepository->isModuleInstalled($this->moduleName)) {
-            $this->info('This module was already installed');
-        } else {
+        $this->install();
 
-            $this->install();
-
-            $this->addToInstalled();
-
-            $this->info('UniSys module '. $this->moduleName .' was installed');
-        }
+        $this->info('UniSys module ' . $this->moduleName . ' was installed');
     }
 
     protected abstract function install();
 
-    private function addToInstalled()
-    {
-        $this->installedModuleRepository->create([
-            'name' => $this->moduleName
-        ]);
-    }
-
     protected function strReplaceInFile($fileName, $find, $replaceWith)
     {
         $content = File::get($fileName);
+
         return File::put($fileName, str_replace($find, $replaceWith, $content));
     }
 }
