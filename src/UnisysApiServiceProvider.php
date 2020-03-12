9f67a153345440724d96c7c021990a378a972380
Vlado Zilka <vlado.zilka@gmail.com>
2020-03-12 08:38:57 +0100

    improve base service provider

--- src/UnisysApiServiceProvider.php
+++ src/UnisysApiServiceProvider.php
@@ -2,17 +2,18 @@
 
 namespace Unite\UnisysApi;
 
-use Route;
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Contracts\Http\Kernel as HttpKernel;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Unite\UnisysApi\Console\Commands\Install\UnisysApiInitializeEnv;
 use Unite\UnisysApi\Console\Commands\Install\UnisysApiInstall;
 use Illuminate\Support\ServiceProvider;
 use Unite\UnisysApi\Console\Commands\MakeModule;
-use Unite\UnisysApi\Providers\GraphQLServiceProvider;
-use Unite\UnisysApi\Providers\ModulesServiceProvider;
-use Unite\UnisysApi\Providers\RouteServiceProvider;
-use Unite\UnisysApi\Providers\ScheduleServiceProvider;
-use Unite\UnisysApi\Providers\MiddlewareServiceProvider;
-use Unite\UnisysApi\Console\Commands\Update;
+use Unite\UnisysApi\Http\Middleware\HttpsProtocol;
 
 class UnisysApiServiceProvider extends ServiceProvider
 {
@@ -21,27 +22,18 @@ class UnisysApiServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(HttpKernel $kernel)
     {
-        Route::patterns([
-            'id'    => '^\d+$',
-            'model' => '^\d+$',
-        ]);
+        $this->setRoutePatterns();
 
-        $this->app->register(RouteServiceProvider::class);
-        $this->app->register(ScheduleServiceProvider::class);
-        $this->app->register(MiddlewareServiceProvider::class);
-        $this->app->register(ModulesServiceProvider::class);
-        $this->app->register(GraphQLServiceProvider::class);
+        $this->loadRoutes();
 
-        $this->commands([
-            UnisysApiInitializeEnv::class,
-            UnisysApiInstall::class,
-            Update::class,
-            MakeModule::class,
-        ]);
+        $this->pushHttpsMiddleware($kernel);
 
         if ($this->app->runningInConsole()) {
+            $this->loadCommands();
+
+            $this->loadSchedules();
 
             $this->publishes([
                 __DIR__ . '/../config/query-filter.php' => config_path('query-filter.php'),
@@ -56,20 +48,90 @@ class UnisysApiServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton(
-            \Illuminate\Contracts\Debug\ExceptionHandler::class,
-            \Unite\UnisysApi\Exceptions\Handler::class
-        );
+        $this->registerModules();
 
-        $this->app->config["filesystems.disks.unisys-api-modules"] = [
-            'driver' => 'local',
-            'root'   => base_path('vendor/weareunite/unisys-api/src/Modules'),
-            'visibility' => 'private',
-        ];
+        $this->registerDisk();
 
         if ($this->app->environment() === 'local') {
-//            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
-            $this->app->register(\Mpociot\ApiDoc\ApiDocGeneratorServiceProvider::class);
+            $this->registerIdeHelper();
         }
+    }
+
+    protected function registerDisk()
+    {
+        Config::set("filesystems.disks.unisys-api-modules", [
+            'driver'     => 'local',
+            'root'       => base_path('vendor/weareunite/unisys-api/src/Modules'),
+            'visibility' => 'private',
+        ]);
+//        $this->app['config']->set("filesystems.disks.unisys-api-modules", [
+//            'driver'     => 'local',
+//            'root'       => base_path('vendor/weareunite/unisys-api/src/Modules'),
+//            'visibility' => 'private',
+//        ]);
+    }
+
+    protected function registerIdeHelper()
+    {
+        $this->app->register(IdeHelperServiceProvider::class);
+    }
+
+    protected function loadCommands()
+    {
+        $this->commands([
+            UnisysApiInitializeEnv::class,
+            UnisysApiInstall::class,
+            MakeModule::class,
+        ]);
+    }
+
+    protected function registerModules()
+    {
+        $directories = Storage::disk('unisys-api-modules')->directories();
+
+        foreach ($directories as $directory) {
+            $this->loadServiceProviderForModule($directory);
+        }
+    }
+
+    private function loadServiceProviderForModule(string $moduleName)
+    {
+        foreach ([ $moduleName, Str::singular($moduleName) ] as $name) {
+            $class = 'Unite\UnisysApi\Modules\\' . $moduleName . '\\' . $name . 'ServiceProvider';
+
+            if (class_exists($class)) {
+                $this->app->register($class);
+                continue;
+            }
+        }
+    }
+
+    protected function loadSchedules()
+    {
+        $this->app->booted(function () {
+            $schedule = $this->app->make(Schedule::class);
+
+            $schedule->command('activitylog:clean')->daily();
+            $schedule->command('auth:clear-resets')->daily();
+
+//            $schedule->command('backup:clean')->daily()->at('01:00');
+//            $schedule->command('backup:run')->daily()->at('02:00');
+        });
+    }
+
+    protected function setRoutePatterns()
+    {
+        Route::pattern('id', '^\d+$');
+        Route::pattern('model', '^\d+$');
+    }
+
+    protected function loadRoutes()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
+    }
+
+    protected function pushHttpsMiddleware(HttpKernel $kernel)
+    {
+        $kernel->pushMiddleware(HttpsProtocol::class);
     }
 }
